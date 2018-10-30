2a4a4a2e70071c3528ba6d72aa8c2e41c82dd996
EastSideCoding <wzhou570@gmail.com>
2018-10-29 23:18:30 -0400

    Added GUI login page for both sender and receiver. Need debugging.

--- Receiver.py
+++ Receiver.py
@@ -1,6 +1,50 @@
 # Receiver prints out the message from Sender
 
 from socket import *
+from tkinter import *
+
+def login_window():
+    # create the main login window
+    window = Tk()
+    window.title("Sender Login Screen")
+    window.geometry("800x800")
+
+    # creates all the labels on the window
+    user_label = Label(window, text = "Username: ")
+    user_label.grid(column = 0, row = 0)
+    pwd_label = Label(window, text = "Password: ")
+    pwd_label.grid(column = 0, row = 1)
+    status_label = Label(window, text = "Status: ")
+    status_label.grid(column = 0, row = 2)
+    result_label = Label(window, text = "Enter credentials")
+    result_label.grid(column = 1, row = 2)
+
+    # creates the username and password entry boxes
+    user_entry = Entry(window, width = 10)
+    user_entry.grid(column = 1, row = 0)
+    pwd_entry = Entry(window, width = 10)
+    pwd_entry.grid(column = 1, row = 1)
+
+    # checks if the credentials are correct
+    def clicked():
+        entered_username = user_entry.get()
+        entered_password = pwd_entry.get()
+        res = ""
+
+        if entered_username != "test" or entered_password != "12345":
+            res = "Invalid credentials."
+            result_label.configure(text = res)
+        else:
+            res = "Successful login!"
+            window.quit() # exits the login window
+            return
+        result_label.configure(text = res)
+
+    # creates the button to analyze the input
+    btn = Button(window, text = "Login", command = clicked)
+    btn.grid(column = 2, row = 1)
+
+    window.mainloop()
 
 serverName = 'localhost'
 serverPort = 12000
@@ -12,6 +56,9 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 # execution of Receiver.py pauses here until it connects to a server
 # (Router.py line 19)
 clientSocket.connect((serverName, serverPort))
+
+# display login GUI
+login_window()
 
 print("Ready to receive.")
 while True:
--- Sender.py
+++ Sender.py
@@ -1,4 +1,48 @@
 from socket import *
+from tkinter import *
+
+def login_window():
+    # create the main login window
+    window = Tk()
+    window.title("Sender Login Screen")
+    window.geometry("800x800")
+
+    # creates all the labels on the window
+    user_label = Label(window, text = "Username: ")
+    user_label.grid(column = 0, row = 0)
+    pwd_label = Label(window, text = "Password: ")
+    pwd_label.grid(column = 0, row = 1)
+    status_label = Label(window, text = "Status: ")
+    status_label.grid(column = 0, row = 2)
+    result_label = Label(window, text = "Enter credentials")
+    result_label.grid(column = 1, row = 2)
+
+    # creates the username and password entry boxes
+    user_entry = Entry(window, width = 10)
+    user_entry.grid(column = 1, row = 0)
+    pwd_entry = Entry(window, width = 10)
+    pwd_entry.grid(column = 1, row = 1)
+
+    # checks if the credentials are correct
+    def clicked():
+        entered_username = user_entry.get()
+        entered_password = pwd_entry.get()
+        res = ""
+
+        if entered_username != "test" or entered_password != "12345":
+            res = "Invalid credentials."
+            result_label.configure(text = res)
+        else:
+            res = "Successful login!"
+            window.quit() # exits the login window
+            return
+        result_label.configure(text = res)
+
+    # creates the button to analyze the input
+    btn = Button(window, text = "Login", command = clicked)
+    btn.grid(column = 2, row = 1)
+
+    window.mainloop()
 
 serverName = 'localhost'
 serverPort = 12000
@@ -9,8 +53,11 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 # initial TCP connection to server
 clientSocket.connect((serverName, serverPort))
 
+# display login GUI
+login_window()
+
 # User input
-messageSend = raw_input("Type in Message: ")
+messageSend = input("Type in Message: ")
 
 if messageSend is not None: # if message is not null
     # message must be encoded first before being sent into the socket
